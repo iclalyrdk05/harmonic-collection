9803f7ca0f245900a1d22582128fb677b5706ec6
iclalyrdk05 <yurdi842@newschool.edu>
2024-11-04 16:32:48 +0000

    herbal tea

---
+++ entry-7/script.js
@@ -0,0 +1,18 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.image-carousel img');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    const carouselContainer = document.querySelector('.carousel-container');
+    const offset = -index * 100; // Move to the left based on the current slide index
+    carouselContainer.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides; // Move to the next slide
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    current
+}
