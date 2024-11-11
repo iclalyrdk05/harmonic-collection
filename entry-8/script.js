ebe0ad7eb5a86ce450d35c0060b2847d2fb14b43
iclalyrdk05 <yurdi842@newschool.edu>
2024-11-11 21:14:01 +0000

    8/1

---
+++ entry-8/script.js
@@ -0,0 +1,22 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel img');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.opacity = i === index ? '1' : '0';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+// Auto-slide functionality
+setInterval(nextSlide, 5000); // Change slide every 5 seconds
